15bab54daf0978268872ccccf1e92decee7f7d5f
arlieboyk <arlieboy.41@gmail.com>
2023-03-30 09:14:03 +0800

    data fetching

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -17,75 +17,28 @@ interface Data {
 }
 
 function Contacts() {
-  const [data, setData] = useState<Data[]>(null);
-  const [isLoading, setLoading] = useState(false);
+  const [data, setData] = useState<Data[]>([]);
+
   useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch("/api/getData", {
-        method: "GET",
-      });
-
-      const data = await res.json();
-      return data;
-    };
-    setLoading(true);
-    // fetchData()
-    //   .then((data) => {
-    //     setData(data);
-    //   })
-    //   .catch((error) => {
-    //     console.error(error);
-    //   });
-    // const res: any = fetchData();
-    // // console.log(res);
-    // const entries = Object.entries(res);
-    // console.log("data ", entries);
-    // setLoading(false);
+    fetch("https://jsonplaceholder.typicode.com/users").then((res) => {
+      res.json().then((res) => setData(res));
+    });
   }, []);
-  //#region
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const res = await fetch("/api/getData", {
-  //       method: "GET",
-  //     });
 
-  //     const data = await res.json();
-  //     return data;
-  //   };
-  //   const res: any = fetchData();
-  //   console.log("res", res);
-  //   setData(res);
-  //   console.log("data ", data);
-  // }, []);
+  useEffect(() => {
+    console.log(data);
+  }, []);
 
-  //#endregion
-
-  // useEffect(() => {
-  //   setLoading(true);
-  //   fetch("/api/getData")
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       setData(data);
-  //       setLoading(false);
-  //     });
-  // }, []);
-
-  if (isLoading) return <p>Loading...</p>;
-  if (!data) return <p>No profile data</p>;
   return (
-    <div>
-      {/* {Array.isArray(data) &&
-        data.map((el) => <div key={el.id}>{el.birthday}</div>)} */}
-      {/* {data.forEach((el) => (
-        <>
-        <h1>{el.fname}</h1>
-        <h1>{el.lname}</h1>
-        </>
-      ))} */}
-      {/* {data[0].confirmpassword} */}
-
-      {/* <GetData/> */}
-    </div>
+    <>
+      <section className="w-full h-full grid grid-flow-col">
+        {data.map((el) => (
+          <div key={el.id} className="">
+            <p className=" min-w-[5rem] text-white font-semibold">{el.name}</p>
+          </div>
+        ))}
+      </section>
+    </>
   );
 }
 
